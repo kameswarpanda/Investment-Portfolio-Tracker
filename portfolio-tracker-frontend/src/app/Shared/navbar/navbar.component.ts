f8dd24f131a33fe7f6c1ac21e4db645d619833d2
Kameswar Panda <kameswarpanda11@gmail.com>
2025-01-25 00:45:55 +0530

    logout functionality fix

--- portfolio-tracker-frontend/src/app/Shared/navbar/navbar.component.ts
+++ portfolio-tracker-frontend/src/app/Shared/navbar/navbar.component.ts
@@ -1,23 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [RouterLinkActive, RouterLink, RouterModule, RouterOutlet],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
   router = inject(Router);
   //for logout
   onLogout(): void {
     localStorage.removeItem('stockUser');
-    this.router.navigateByUrl('/home/login');
-    window.location
+    this.router.navigateByUrl('/home');
+    window.location;
   }
-  
-  //for dark mode 
+
+  //for dark mode
   darkMode = false; // Toggle state
 
   toggleDarkMode(): void {
@@ -29,5 +34,4 @@ export class NavbarComponent {
       body.classList.remove('dark-mode');
     }
   }
-
 }
