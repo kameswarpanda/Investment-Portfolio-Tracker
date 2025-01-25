8b37129091ba72c1bffa2b46574574839355c71b
Kameswar Panda <kameswarpanda11@gmail.com>
2025-01-26 05:10:48 +0530

    UI finishing

--- portfolio-tracker-frontend/src/app/Pages/stocks/stocks.component.ts
+++ portfolio-tracker-frontend/src/app/Pages/stocks/stocks.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-stocks',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './stocks.component.html',
   styleUrl: './stocks.component.css'
 })
