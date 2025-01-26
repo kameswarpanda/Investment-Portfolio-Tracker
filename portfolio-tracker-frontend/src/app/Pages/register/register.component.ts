c45c7d352759d14f42b3704f7a849c595acbda43
Kameswar Panda <kameswarpanda11@gmail.com>
2025-01-27 04:34:01 +0530

    sweetalert added

--- portfolio-tracker-frontend/src/app/Pages/register/register.component.ts
+++ portfolio-tracker-frontend/src/app/Pages/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { MasterService } from '../../services/master.service';
 import Swal from 'sweetalert2';
+import { error } from 'console';
 
 @Component({
   selector: 'app-register',
@@ -21,44 +22,85 @@ export class RegisterComponent {
   router = inject(Router);
 
   onSubmit(): void {
-    this.MasterSrv.register(this.registrationObj).subscribe((res: any) => {
-      if (res.id != null) {
-        // alert('Registration successful! Login Now!');
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-          }
-        });
-        Toast.fire({
-          icon: "success",
-          title: "Registration successful, Login Now!"
-        });
-        
-        this.router.navigateByUrl('/home/login');
-      } else {
+    if (
+      this.registrationObj.email == '' ||
+      this.registrationObj.fullName == '' ||
+      this.registrationObj.password == ''
+    ) {
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
+      });
+      Toast.fire({
+        icon: 'error',
+        title: 'Please fill all the fields.',
+      });
+      return;
+    } else {
+      this.MasterSrv.register(this.registrationObj).subscribe((res: any) => {
+        if (res.id != null) {
+          // alert('Registration successful! Login Now!');
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            },
+          });
+          Toast.fire({
+            icon: 'success',
+            title: 'Registration successful, Login Now!',
+          });
+
+          this.router.navigateByUrl('/home/login');
+        } else {
+          // On failure
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+              toast.onmouseenter = Swal.stopTimer;
+              toast.onmouseleave = Swal.resumeTimer;
+            },
+          });
+          Toast.fire({
+            icon: 'error',
+            title: 'something went wrong, please try again.',
+          });
+        }
+      },error => {
         // On failure
         const Toast = Swal.mixin({
           toast: true,
-          position: "top-end",
+          position: 'top-end',
           showConfirmButton: false,
           timer: 3000,
           timerProgressBar: true,
           didOpen: (toast) => {
             toast.onmouseenter = Swal.stopTimer;
             toast.onmouseleave = Swal.resumeTimer;
-          }
+          },
         });
         Toast.fire({
-          icon: "error",
-          title: "something went wrong, please try again."
+          icon: 'error',
+          title: 'something went wrong, please try again.',
         });
       }
-    });
+    );
+    }
   }
 }
